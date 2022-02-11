cc0dd025a1263b4789b2a8157273d8859d411f10
Cooper Webb <CCWebb14@gmail.com>
2022-02-11 12:36:19 -0800

    toggling color on click of box

--- switchScript.js
+++ switchScript.js
@@ -1,1 +1,10 @@
-const keys = document.querySelectorAll('.key');
+const lights = document.querySelectorAll('.light');
+console.log(lights);
+
+lights.forEach(light => light.addEventListener('click', switchLight));
+
+function switchLight(e){
+    this.classList.toggle('on');
+}
+
+switchLight
